dc1f3a633d25177ef8778afbc098c44c6c68511d
ponomaryovigor <ponomaryovigor@skbkontur.ru>
2018-11-02 10:46:53 +0500

    Fix rebase conflict

---
+++ Exceptions/UnknownSqlStorageException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SKBKontur.Catalogue.EDI.SqlStorageCore.Exceptions
+{
+    public class UnknownSqlStorageException : SqlStorageException
+    {
+        public UnknownSqlStorageException(Exception innerException)
+            : base("Unknown exception in SqlStorage", innerException)
+        {
+        }
+    }
+}
