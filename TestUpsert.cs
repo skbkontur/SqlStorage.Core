9def8955b9188f83068ac6e5d28ba5a3ef2269af
ponomaryovigor <ponomaryovigor@skbkontur.ru>
2019-01-27 13:40:57 +0500

    Add some upsert tests

--- TestUpsert.cs
+++ TestUpsert.cs
@@ -41,7 +41,7 @@ namespace SKBKontur.EDIFunctionalTests.SqlStorageCoreTests
         }
 
         [Test]
-        public void TestUpsertOnUniqueConstraintThrows()
+        public void CreateEntity_WithDuplicateUniqueConstraint_Throws()
         {
             var entity1 = GenerateObjects().First();
             var entity2 = GenerateObjects().First();
@@ -53,13 +53,37 @@ namespace SKBKontur.EDIFunctionalTests.SqlStorageCoreTests
         }
 
         [Test]
-        public void UpsertEntity_WithoutRequiredValue_Throws()
+        public void UpdateEntity_WithDuplicateUniqueConstraint_Throws()
+        {
+            var entity1 = GenerateObjects().First();
+            var entity2 = GenerateObjects().First();
+            sqlStorage.CreateOrUpdate(new[] {entity1, entity2});
+
+            (entity2.SomeId1, entity2.SomeId2) = (entity1.SomeId1, entity1.SomeId2);
+            Action updating = () => sqlStorage.CreateOrUpdate(entity2);
+
+            updating.Should().Throw<UniqueViolationException>();
+        }
+
+        [Test]
+        public void CreateEntity_WithoutRequiredValue_Throws()
         {
             var entity = GenerateObjects().First();
             entity.RequiredValue = null;
 
-            Action saving = () => sqlStorage.CreateOrUpdate(entity);
-            saving.Should().Throw<UnknownSqlStorageException>();
+            Action creating = () => sqlStorage.CreateOrUpdate(entity);
+            creating.Should().Throw<UnknownSqlStorageException>();
+        }
+
+        [Test]
+        public void UpdateEntity_WithoutRequiredValue_Throws()
+        {
+            var entity = GenerateObjects().First();
+            sqlStorage.CreateOrUpdate(entity);
+
+            entity.RequiredValue = null;
+            Action updating = () => sqlStorage.CreateOrUpdate(entity);
+            updating.Should().Throw<UnknownSqlStorageException>();
         }
 
         [Test]
