c1f8a046bffed721dc85caef5039cbec1a810143
iperevoschikov <iperevoschikov@skbkontur.ru>
2020-03-06 11:01:09 +0500

    fix migrations generation

--- add-tests-migration.bat
+++ add-tests-migration.bat
@@ -1,4 +1,5 @@
 @echo off
+dotnet tool restore
 set /p migration="Enter migration name: "
 dotnet ef migrations add --context SqlDbContext --project SqlStorageCoreTests --startup-project SqlStorageCoreTests --verbose %migration%
 pause
